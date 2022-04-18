67293f7c6b6f14154db48c607b7e33f5c5160bb8
Pedro-Corbari <pedrocorbari.dev@gmail.com>
2022-04-18 19:14:24 -0300

    LastUpdate

--- 03 - Codigos Arduino Mega/03_02 - VersoesTestes/V1/V1.ino
+++ 03 - Codigos Arduino Mega/03_02 - VersoesTestes/V1/V1.ino
@@ -12,24 +12,39 @@ DHT dht(DHTPIN, DHTTYPE);
 //Define Moisture Sensor
 #define pino_sinal_analogico A0
 int MoistureSensor;
+int HP;
+
+//Define LDR Sensor
+const int PinoLDR = A5;
+int LDRSensor;
+int LP;
+
+int Umidade () {
+  HP = (MoistureSensor/1024)*100;
+  return HP;
+}
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
-  pinMode(pino_sinal_analogico, INPUT);
-
+  pinMode(pino_sinal_analogico, INPUT); //Sensor de Umidade do solo
+  pinMode(PinoLDR, INPUT); //Sensor de luminosidade
 }
 
 void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
+  //Le o valor do pino A5 do LDR
+  LDRSensor = analogRead(PinoLDR);
+
   //Le o valor do pino A0 do Moisture sensor
   MoistureSensor = analogRead(pino_sinal_analogico);
 
 
-  String outString = "{\"method\": \"detail\", \"json\": {\"plantId\":1,\"internalHumidity\":32,\"externalHumidity\":33,\"temperature\":45,\"luminosity\":23}}" ;
-//String outString = "{\"method\": \"detail\", \"json\": {\"name\": \"MEU NOME DO ARDUINO\", \"leitSensor\":" + String(t) + ",  }}" ; //Soma string com a leitura do sensor  
+
+  String outString = "{\"method\": \"detail\", \"json\": {\"plantId\":1,\"internalHumidity\":" + String(Umidade()) + ",\"externalHumidity\":" + String(h) + ",\"temperature\":" + String(t) + ",\"luminosity\":" + String(LDRSensor) + "}}" ;
+//  String outString = "{\"method\": \"detail\", \"json\": {\"name\": \"MEU NOME DO ARDUINO\", \"leitSensor\":" + String(t) + ",  }}" ; //Soma string com a leitura do sensor  
  Serial.println(outString); 
  delay (1000);
 }
